4adba42fa1cdcbb2ca3ea476b11d68af20036ec2
Galaxy trading <228169516+rodolphetenpel054-jpg@users.noreply.github.com>
2025-12-22 19:07:05 +0100

    Correct syntax and structure in Withdrawale.js
    Fix syntax errors and improve formatting in Withdrawale.js.

---
+++ Api/Deposit/Withdrawale.js
@@ -0,0 +1,8 @@
+js
+export défaut function
+handler(req,res){return: statuts(200).json({success:true, message :true
+                                            message : deposit Api actve.});
+js
+export défaut function
+handler(req,res)} returnn: statuts (200).json({success:true, message : withdrawale.Api active})
+                                              }
